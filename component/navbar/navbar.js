20f13f1b45e1039fad27e70033d0a2b0d292954e
JoeLeelyf <l-yf21@mails.tsinghua.edu.cn>
2023-05-13 20:07:53 +0800

    Test Slider

--- component/navbar/navbar.js
+++ component/navbar/navbar.js
@@ -10,19 +10,10 @@ xhr.onload = function () {
   if (xhr.status === 200) {
     // 将读取到的内容插入到navbarContainer中
     navbarContainer.innerHTML = xhr.responseText;
-
-    // 判断当前页面，将对应的导航栏菜单项加粗
-    const currentPageUrl = window.location.href;
-    const navItems = navbarContainer.querySelectorAll('.navbar-item');
-    for (let i = 0; i < navItems.length; i++) {
-      const navItemUrl = navItems[i].getAttribute('data-url');
-      if (navItemUrl === currentPageUrl) {
-        navItems[i].classList.add('active');
-      }
-    }
   }
 };
 xhr.send();
 
 // 插入navbarContainer到页面中
 document.body.insertBefore(navbarContainer, document.getElementById('main-container'));
+
