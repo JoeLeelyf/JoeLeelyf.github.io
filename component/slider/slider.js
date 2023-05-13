01ab526d82ce0dc31ba85c39f12b0e09d4d0b172
JoeLeelyf <l-yf21@mails.tsinghua.edu.cn>
2023-05-13 22:17:42 +0800

    Test Slider

--- component/slider/slider.js
+++ component/slider/slider.js
@@ -1,5 +1,7 @@
 const slideshow = document.querySelector('.slideshow');
-const slideshowImages = document.querySelectorAll('.slideshow img');
+const slideshowImages = document.querySelectorAll('.slideshow .slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
 let slideIndex = 0;
 let slideInterval = null;
@@ -27,9 +29,25 @@ function stopSlideshow() {
   clearInterval(slideInterval);
 }
 
+// 切换到前一张幻灯片
+function prevSlide() {
+  slideIndex = (slideIndex - 1 + slideshowImages.length) % slideshowImages.length;
+  showSlides();
+}
+
+// 切换到下一张幻灯片
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slideshowImages.length;
+  showSlides();
+}
+
 // 开始轮播
 startSlideshow();
 
 // 绑定鼠标悬停事件
 slideshow.addEventListener('mouseenter', stopSlideshow);
 slideshow.addEventListener('mouseleave', startSlideshow);
+
+// 绑定点击事件
+prevButton.addEventListener('click', prevSlide);
+nextButton.addEventListener('click', nextSlide);
