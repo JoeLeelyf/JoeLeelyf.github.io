033a8094c4e15167aa3f115dffc60b155bd59017
JoeLeelyf <l-yf21@mails.tsinghua.edu.cn>
2023-05-14 14:04:33 +0800

    Nearly Done

---
+++ js/arrow.js
@@ -0,0 +1,7 @@
+var arrowContainer = document.getElementById("arrow-container");
+
+arrowContainer.addEventListener("click", function() {
+  // 点击后执行跳转操作
+  window.location.href = "year_2021.html";
+});
+
