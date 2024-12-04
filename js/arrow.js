f92579fbf9dcdcafebb566fe031b54fd2addafbb
李逸飞 <liyifei@liyifeidebijibendiannao.local>
2024-12-04 21:19:39 +0800

    del all

--- js/arrow.js
+++
@@ -1,7 +0,0 @@
-var arrowContainer = document.getElementById("arrow-container");
-
-arrowContainer.addEventListener("click", function() {
-  // 点击后执行跳转操作
-  window.location.href = "year_2021.html";
-});
-
